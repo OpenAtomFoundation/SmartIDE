1121b3d7e19f8655a96ca7fed968a82ee29b6b4c
jasonchen <jasonchen@leansoftx.com>
2021-09-30 12:54:38 +0800

    🐛 port is used

--- lib/common/network.go
+++ lib/common/network.go
@@ -28,16 +28,22 @@ func GetAvailablePort() (int, error) {
 // 判断端口是否可以（未被占用）
 func IsPortAvailable(port int) bool {
 
-	address := fmt.Sprintf("%s:%d", "0.0.0.0", port)
-	listener, err := net.Listen("tcp", address)
+	address := fmt.Sprintf("%s:%d", "localhost", port)
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", "localhost", port))
 	if err != nil {
-
-		SmartIDELog.Warning(fmt.Sprintf("port %s is taken: %s", address, err))
-
+		SmartIDELog.Warning(fmt.Sprintf("tcp port %s is taken: %s", address, err))
 		return false
 	}
-
 	defer listener.Close()
+
+	address = fmt.Sprintf("%s:%d", "0.0.0.0", port)
+	conn, err := net.Listen("tcp", address)
+	if err != nil {
+		SmartIDELog.Warning(fmt.Sprintf("tcp port %s is taken: %s", address, err))
+		return false
+	}
+	defer conn.Close()
+
 	return true
 }
 
