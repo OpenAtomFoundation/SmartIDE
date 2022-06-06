9578430de08166a0f51e877159a6437970476354
赵 筱彤 <zhaoxiaotong@leansoftx.com>
2022-06-06 12:12:52 +0000

    Updated startup-openvscode.sh

--- dev-containers/smartide-base-v2-vscode/startup-openvscode.sh
+++ dev-containers/smartide-base-v2-vscode/startup-openvscode.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-exec /home/opvscode/bin/openvscode-server --host 0.0.0.0 --without-connection-token
+/home/smartide/.nvm/versions/node/v16.9.1/bin/node /home/opvscode/out/server-main.js --host 0.0.0.0 --without-connection-token
