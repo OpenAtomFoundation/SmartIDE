244c0c0080e879a81b0075392287eb5255876124
leebchi <chileeb@163.com>
2022-07-22 10:59:53 +0800

    调整脚本路径

--- deployment/k8s/smartide-k8s-init.sh
+++ deployment/k8s/smartide-k8s-init.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
 # Author: SmartIDE
 # Github: https://github.com/SmartIDE/SmartIDE
 
-
+#!/bin/bash
 # K8S Ingress服务及Service配置
-kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller.yaml
-kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller-service.yaml
+kubectl apply -f https://gitee.com/smartide/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller.yaml
+kubectl apply -f https://gitee.com/smartide/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller-service.yaml
 # K8S Service证书配置
-kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/cert-manager/cert-manager.yaml
-kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/cert-manager/cluster-issuer.yaml
+kubectl apply -f https://gitee.com/smartide/SmartIDE/raw/main/deployment/k8s/cert-manager/cert-manager.yaml
+kubectl apply -f https://gitee.com/smartide/SmartIDE/raw/main/deployment/k8s/cert-manager/cluster-issuer.yaml
 # K8S StroageClass配置
-kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/file-storageclass/smartide-file-storageclass.yaml
+kubectl apply -f https://gitee.com/smartide/SmartIDE/raw/main/deployment/k8s/file-storageclass/smartide-file-storageclass.yaml
+
