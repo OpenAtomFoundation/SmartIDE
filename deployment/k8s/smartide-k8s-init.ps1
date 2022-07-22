c4734cbb15ef0fbcbaef31036dd5659d6b206891
冀利斌 <jilibin@leansoftx.com>
2022-07-22 01:24:04 +0000

    Merged PR 906: add k8s init script && modify deployment path
    add k8s init script && modify deployment path。
    K8S初始化资源环境使用脚本，这里包括shell脚本及ps脚本。
    合并后会去更新对应文档：https://leansoftx.feishu.cn/wiki/wikcncC7guzEZRNAJxKCSmtjwBg
    
    Related work items: #1341

---
+++ deployment/k8s/smartide-k8s-init.ps1
@@ -0,0 +1,11 @@
+# Author: SmartIDE
+# Github: https://github.com/SmartIDE/SmartIDE
+
+# K8S Ingress服务及Service配置
+kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller.yaml
+kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/ingress-controller/ingress-controller-service.yaml
+# K8S Service证书配置
+kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/cert-manager/cert-manager.yaml
+kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/cert-manager/cluster-issuer.yaml
+# K8S StroageClass配置
+kubectl apply -f https://gitee.com/chileeb/SmartIDE/raw/main/deployment/k8s/file-storageclass/smartide-file-storageclass.yaml
