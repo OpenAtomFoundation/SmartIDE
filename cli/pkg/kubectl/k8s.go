87c8491666a27bda47886224513a1bc75b17f414
jasonchen <jasonchen@leansoftx.com>
2022-08-02 15:57:36 +0800

    windows 中嵌套“改为`

--- cli/pkg/kubectl/k8s.go
+++ cli/pkg/kubectl/k8s.go
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-03-23 16:13:54
  * @LastEditors: Jason Chen
- * @LastEditTime: 2022-08-02 14:43:33
+ * @LastEditTime: 2022-08-02 15:56:59
  * @FilePath: /smartide/cli/pkg/kubectl/k8s.go
  */
 
@@ -440,7 +440,11 @@ func (k *KubernetesUtil) GetPod(selector string, namespace string) (*coreV1.Pod,
 func (k *KubernetesUtil) ExecuteCommandRealtimeInPod(pod coreV1.Pod, command string, runAsUser string) error {
 	//command = "su smartide -c " + command
 	if runAsUser != "" && runAsUser != "root" {
-		command = strings.ReplaceAll(command, "'", "\"\"")
+		if runtime.GOOS == "windows" {
+			command = strings.ReplaceAll(command, "'", "`")
+		} else {
+			command = strings.ReplaceAll(command, "'", "\"\"")
+		}
 		command = fmt.Sprintf(`su %v -c '%v'`, runAsUser, command)
 	}
 	kubeCommand := fmt.Sprintf(` -it exec %v -- /bin/bash -c "%v"`, pod.Name, command)
@@ -457,7 +461,11 @@ func (k *KubernetesUtil) ExecuteCommandRealtimeInPod(pod coreV1.Pod, command str
 func (k *KubernetesUtil) ExecuteCommandCombinedInPod(pod coreV1.Pod, command string, runAsUser string) (string, error) {
 	//command = "su smartide -c " + command
 	if runAsUser != "" && runAsUser != "root" {
-		command = strings.ReplaceAll(command, "'", "\"\"")
+		if runtime.GOOS == "windows" {
+			command = strings.ReplaceAll(command, "'", "`")
+		} else {
+			command = strings.ReplaceAll(command, "'", "\"\"")
+		}
 		command = fmt.Sprintf(`su %v -c '%v'`, runAsUser, command)
 	}
 	kubeCommand := fmt.Sprintf(` -it exec %v -- /bin/bash -c "%v"`, pod.Name, command)
@@ -469,7 +477,11 @@ func (k *KubernetesUtil) ExecuteCommandCombinedInPod(pod coreV1.Pod, command str
 func (k *KubernetesUtil) ExecuteCommandCombinedBackgroundInPod(pod coreV1.Pod, command string, runAsUser string) {
 	//command = fmt.Sprintf("su smartide -c '%v'", command)
 	if runAsUser != "" && runAsUser != "root" {
-		command = strings.ReplaceAll(command, "'", "\"\"")
+		if runtime.GOOS == "windows" {
+			command = strings.ReplaceAll(command, "'", "`")
+		} else {
+			command = strings.ReplaceAll(command, "'", "\"\"")
+		}
 		command = fmt.Sprintf(`su %v -c '%v'`, runAsUser, command)
 	}
 	kubeCommand := fmt.Sprintf(` exec  %v -- /bin/bash -c "%v"`, pod.Name, command)
