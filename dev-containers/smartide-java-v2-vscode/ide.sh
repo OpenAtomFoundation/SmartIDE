f80e67a90063a658e9136788b98e4cb0c773e05e
lixiaoming <lixiaoming@leansoftx.com>
2022-09-21 11:35:08 +0800

    up ide.sh vmlc

--- dev-containers/smartide-java-v2-vscode/ide.sh
+++ dev-containers/smartide-java-v2-vscode/ide.sh
@@ -1,22 +1,26 @@
 
 ###
  # @Author: kenan
- # @Date: 2022-05-24 14:37:27
+ # @Date: 2022-07-07 14:24:14
  # @LastEditors: kenan
- # @LastEditTime: 2022-07-11 15:32:13
- # @FilePath: /smartide/dev-containers/smartide-node-v2-vscode/ide.sh
+ # @LastEditTime: 2022-07-08 17:40:52
+ # @FilePath: /smartide/dev-containers/smartide-base-v2-vscode/ide.sh
  # @Description: 
  # 
  # Copyright (c) 2022 by kenanlu@leansoftx.com, All Rights Reserved. 
 ### 
 echo 'ide.sh............start'
 
-echo 'ide.sh............start'
-
 if [ -d "./openvscode-images-amd64/" ];then
  sudo rm -rf openvscode-images-amd64
 else
   echo 'openvscode-server............不存在'
+fi
+
+if [ -d "./openvscode-images-vmlc/" ];then
+ sudo rm -rf openvscode-images-vmlc
+else
+  echo 'openvscode-server-vmlc.............不存在'
 fi
 
 if [ -d "./openvscode-images-arm64/" ];then
@@ -31,8 +35,9 @@ else
   echo 'vsix...........不存在'
 fi
 
-sudo mkdir openvscode-images-amd64 openvscode-images-arm64 vsix vsix/extensions
+sudo mkdir openvscode-images-amd64 openvscode-images-vmlc openvscode-images-arm64 vsix vsix/extensions
 sudo chmod -R 777 openvscode-images-amd64
+sudo chmod -R 777 openvscode-images-vmlc
 sudo chmod -R 777 openvscode-images-arm64
 sudo chmod -R 777 vsix
 sudo chmod -R 777 vsix/extensions
@@ -40,17 +45,13 @@ sudo chmod -R 777 vsix/extensions
 
 # 解压目录
 sudo tar -zxf #{OpenVScodeServerVmlcFileName}#.tar.gz --strip-components 1 -C openvscode-images-amd64
+sudo tar -zxf #{OpenVScodeServerVmlcFileName}#.tar.gz --strip-components 1 -C openvscode-images-vmlc
 sudo tar -zxf #{OpenVScodeServerVmlcFileName}#-arm64.tar.gz --strip-components 1 -C openvscode-images-arm64
 
 # 删除node   
 sudo rm -rf ./openvscode-images-amd64/node
+sudo rm -rf ./openvscode-images-vmlc/node
 sudo rm -rf ./openvscode-images-arm64/node
-
-# 删除server.sh
-# sudo rm -rf ./openvscode-images/server.sh
-# 复制server.sh
-# sudo cp server.sh ./openvscode-images/
-# sudo chmod +x ./openvscode-images/server.sh;
 
 
 # 解压插件
@@ -65,6 +66,7 @@ for i in ./extensions/*.vsix;
     done
 
 sudo \cp -rf ./vsix/extensions openvscode-images-amd64
+sudo \cp -rf ./vsix/extensions  openvscode-images-vmlc
 sudo \cp -rf ./vsix/extensions  openvscode-images-arm64
 
 echo 'ide.sh............end'
