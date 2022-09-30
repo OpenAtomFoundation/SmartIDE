41827f4530697e5f4f4a2a3009ab586909b93ca7
limin <limin@smartide>
2022-09-30 04:22:19 +0000

    add net7.0

---
+++ dev-containers/smartide-dotnet-v2/7.0/install-dotnet.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-6.0
